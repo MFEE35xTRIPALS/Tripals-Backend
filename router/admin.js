c9f5385871acf72995de6ef821d51e198af40f66
Tina <t2239tina@gmail.com>
2023-04-22 03:04:03 +0800

    admin 最新消息OK
    最新消息CRUD完成

--- router/admin.js
+++ router/admin.js
@@ -3,13 +3,32 @@ var page = express.Router();
 // -----------------------------------
 // 連線資料庫 port 3306
 // -----------------------------------
+// var mysql = require("mysql");
+// var connhelper = mysql.createConnection({
+//   host: "192.3.80.70",
+//   port: 3306,
+//   user: "admin",
+//   password: "P@ssw0rd",
+//   database: "db_tripals",
+//   multipleStatements: true,
+// });
+// connhelper.connect(function (err) {
+//   if (err) {
+//     console.log("資料庫連線錯誤", err.sqlMessage);
+//   } else {
+//     console.log("資料庫連線成功");
+//   }
+// });
+// -----------------------------------
+// 引用mysql套件
 var mysql = require("mysql");
+// 填入連線資料庫所需資料
 var connhelper = mysql.createConnection({
-  host: "192.3.80.70",
-  port: 3306,
-  user: "admin",
-  password: "P@ssw0rd",
-  database: "db_tripals",
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "lab",
   multipleStatements: true,
 });
 connhelper.connect(function (err) {
@@ -27,33 +46,76 @@ page.get("/", function (req, res) {
 
   connhelper.query(sql, [], function (err, result, fields) {
     if (err) {
-      res.send("select 發生錯誤", err);
+      res.send("MySQL 可能語法寫錯了", err);
     } else {
       res.json(result);
-      //   res.json({ apple: 100, bee: 200, cat: 300, dog: 400 });
     }
   });
 });
 
 /* POST */
-page.post("/", express.urlencoded(), function (req, res) {
+page.post("/post", express.urlencoded(), function (req, res) {
   console.log(req.body);
   var sql =
-    "INSERT INTO `tb_news`(`userno`,`title`, `content`, `release`) VALUES (1,'?','?','?')";
+    "INSERT INTO `tb_news`(`userno`, `title`, `content`, `release`)  VALUES (1,?,?,?);";
   var sqlAll =
     "SELECT newsno,title, content, DATE_FORMAT(`release`, '%Y-%m-%d') `date` FROM `tb_news`;";
   // // 這邊userno 先固定1->屆時要回來調整
+  // console.log(sql);
   connhelper.query(
-    sqlAll,
-
+    sql + sqlAll,
     [req.body.title, req.body.content, req.body.release],
     function (err, results, fields) {
       if (err) {
-        res.send("select 發生錯誤", err);
+        res.send("MySQL 可能語法寫錯了", err);
+      } else {
+        console.log(results[0]);
+        res.json(results[1]);
+      }
+    }
+  );
+});
+
+/* UPDATE */
+page.post("/update", express.urlencoded(), function (req, res) {
+  console.log(req.body);
+  var sql =
+    "UPDATE `tb_news` SET `title`=?,`content`=?,`release`=?,date=now() WHERE `newsno`=?;";
+  var sqlAll =
+    "SELECT newsno,title, content, DATE_FORMAT(`release`, '%Y-%m-%d') `date` FROM `tb_news`;";
+  // // 這邊userno 先固定1->屆時要回來調整
+  // console.log(sql);
+  connhelper.query(
+    sql + sqlAll,
+    [req.body.title, req.body.content, req.body.release, req.body.newsno],
+    function (err, results, fields) {
+      if (err) {
+        res.send("MySQL 可能語法寫錯了", err);
       } else {
         console.log(results);
-        res.json(results);
-        //   res.json({ apple: 100, bee: 200, cat: 300, dog: 400 });
+        res.json(results[1]);
+      }
+    }
+  );
+});
+
+/* DELETE */
+page.post("/delete", express.urlencoded(), function (req, res) {
+  console.log(req.body);
+  var sql = "DELETE FROM `tb_news` WHERE newsno = ?;";
+  var sqlAll =
+    "SELECT newsno,title, content, DATE_FORMAT(`release`, '%Y-%m-%d') `date` FROM `tb_news`;";
+  // // 這邊userno 先固定1->屆時要回來調整
+  // console.log(sql);
+  connhelper.query(
+    sql + sqlAll,
+    [req.body.newsno],
+    function (err, results, fields) {
+      if (err) {
+        res.send("MySQL 可能語法寫錯了", err);
+      } else {
+        console.log(results[0]);
+        res.json(results[1]);
       }
     }
   );
