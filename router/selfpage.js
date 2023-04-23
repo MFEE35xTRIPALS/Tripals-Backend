0d10269d443497f11c1af75fdb33ccd3efb1d078
saruhiko80155 <saruhiko80155@gmail.com>
2023-04-23 21:41:07 +0800

    按錯了...

--- router/selfpage.js
+++ router/selfpage.js
@@ -6,7 +6,7 @@ var connhelper = require("./config");
 
 // select
 page.get('/cards', function (req, res) {
-    var sqlall = "SELECT `collect` FROM  `tb_user` WHERE userno=2;";//?接收使用這輸入資料
+    var sqlall = "SELECT `collect` FROM  `tb_user` WHERE userno=2;";//?接收登入者的id
     var sql = "SELECT `intro`,`banner`,`articleno`,`nickname`,`collect`,`avatar`,`tb_main_article`.`userno`,`title`,`image`,`view_count`,`like_count` FROM `tb_main_article` JOIN `tb_user` ON `tb_user`.`userno`=`tb_main_article`.`userno` WHERE `tb_main_article`.`userno`=3;";//?接收使用這輸入資料
     connhelper.query(sql+sqlall,
         [req.params.userno],//填入？的位置
@@ -15,7 +15,7 @@ page.get('/cards', function (req, res) {
                 res.send('select發生錯誤', err);
             } else {
                 res.json(result);
-                console.log("done");
+                // console.log("done");
 
             }
         });
