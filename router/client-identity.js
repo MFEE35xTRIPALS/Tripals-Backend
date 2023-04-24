085ba2587c5008c56525890c7f4a24c3586732b6
Tina <t2239tina@gmail.com>
2023-04-24 15:18:54 +0800

    將userno設定為變數
    之後可能使用session還是什麼的抓

--- router/client-identity.js
+++ router/client-identity.js
@@ -8,15 +8,17 @@ var connhelper = require("./config");
 //
 
 // --------------- 這裡是個人資料讀取 --------------------
-// 還未設定userno=2
+// 還未設定userno=6
 /* GET */
 // ---------------//
 page.get("/identity", function (req, res) {
+  // console.log(req.body.userno);
   var sql =
     "SELECT  `userno`,`id`, `password`, `nickname`, DATE_FORMAT(`birthday`, '%Y-%m-%d')`birthday`, `intro`,SUBSTRING_INDEX(`id`, '@', 1)`username` FROM `tb_user` WHERE userno=6;";
 
   connhelper.query(sql, [], function (err, result, fields) {
     if (err) {
+      // console.log(req.body.userno);
       res.send("MySQL 可能語法寫錯了", err);
     } else {
       res.json(result);
@@ -27,15 +29,18 @@ page.get("/identity", function (req, res) {
 /* POST */
 //---------
 page.post("/identity/update", express.urlencoded(), function (req, res) {
-  console.log(req.body);
+  // console.log(req.body.userno);
   // mysql 格式 date 要填空值時，要填null
   var birthday = req.body.birthday ? req.body.birthday : null;
   var sql =
     "UPDATE `tb_user` SET `id`=?,`password`=?,`nickname`=?,`birthday`=?, `intro`=?,date=now() WHERE `userno`=?;";
 
   var sqlAll =
-    "SELECT  `userno`,`id`, `password`, `nickname`, DATE_FORMAT(`birthday`, '%Y-%m-%d')`birthday`, `intro`,SUBSTRING_INDEX(`id`, '@', 1)`username` FROM `tb_user` WHERE userno=6;";
-  // console.log(sql);
+    "SELECT  `userno`,`id`, `password`, `nickname`, DATE_FORMAT(`birthday`, '%Y-%m-%d')`birthday`, `intro`,SUBSTRING_INDEX(`id`, '@', 1)`username` FROM `tb_user` WHERE userno=6 ;";
+  console.log("req有抓到nickname" + req.body.nickname);
+  console.log("req有抓到userno" + req.body.userno);
+  console.log("req有抓到intro" + req.body.intro);
+  console.log("req有抓到birthday" + birthday);
   connhelper.query(
     sql + sqlAll,
     [
@@ -50,10 +55,24 @@ page.post("/identity/update", express.urlencoded(), function (req, res) {
       if (err) {
         res.send("MySQL 可能語法寫錯了", err);
       } else {
+        console.log("結果在這ㄌ裡");
         console.log(results[1]);
         res.json(results[1]);
       }
     }
   );
 });
+// --------------- 這裡是照片讀取 --------------------
+// page.get("/img", function (req, res) {
+//   res.sendFile(__dirname + "/p11.html");
+// });
+// //------------
+// var bodyParser = require("body-parser");
+// page.use(bodyParser.urlencoded()); // 全域變數
+// // app.use(bodyParser.json());
+// page.post("/avatar", function (req, res) {
+//   console.log(req.body);
+//   res.send("好好");
+// });
+
 module.exports = page;
