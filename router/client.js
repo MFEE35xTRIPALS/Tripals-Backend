5f17b46d8b3fb71adbf16f228693a1a1e69e24b3
saruhiko80155 <saruhiko80155@gmail.com>
2023-04-25 15:52:02 +0800

    大頭貼快要完成了

--- router/client.js
+++ router/client.js
@@ -12,24 +12,33 @@ var mystorage = multer.diskStorage({
     },
     filename: function (req, file, cb) {//編寫檔案名稱
         // var userFileName = Date.now() + '-' + file.originalname;//留下檔案戳記記錄歷程
-        var userFileName='2'+'.'+file.originalname.split('.')[1];//留下自己可辨別的檔案
+        var userFileName = '2' + '.' + file.originalname.split('.')[1];//留下自己可辨別的檔案
         cb(null, userFileName);
     }
 })
 let upload = multer({
     storage: mystorage,
     fileFilter: function (req, file, cb) {
-        if (file.mimetype == 'image/png'||file.mimetype == 'image/jpg'||file.mimetype == 'image/jpeg'||file.mimetype == 'image/gif') {
+        if (file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg' || file.mimetype == 'image/gif') {
             cb(null, true)
         } else {
-           return cb(new Error('上傳檔案類型錯誤'))
+            return cb(new Error('上傳檔案類型錯誤'))
         }
     }
 });
-page.post('/upload', upload.single('shotUpload'), function(req, res) {
-    res.send("大頭貼修改完成");
+page.post('/upload', upload.single('shotUpload'), function (req, res) {
+    let sql = `UPDATE tb_user SET avatar = '/useravatar/2.${req.file.originalname.split('.')[1]}' WHERE userno = 2;`;
+    connhelper.query(sql,[], (err, results, fields) => {
+        if (err) {
+            res.send("MySQL 可能語法寫錯了", err);
+        } else {
+            // console.log(results);
+            res.send('大頭貼修改完成');
+        }
+    });
     // console.log(req.file);
-  });
+});
+
 
 //select
 page.get('/avatar', function (req, res) {
@@ -40,6 +49,7 @@ page.get('/avatar', function (req, res) {
             if (err) {
                 res.send('select發生錯誤', err);
             } else {
+                // console.log('done');
                 res.json(result[0]);
             }
         });
