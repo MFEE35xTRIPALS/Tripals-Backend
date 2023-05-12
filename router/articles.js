fa4844fb76ee9ab487e1172a7d9686adf7f06975
Tina <t2239tina@gmail.com>
2023-05-13 01:30:49 +0800

    articles 小調整

--- router/articles.js
+++ router/articles.js
@@ -11,60 +11,60 @@ var connhelper = require("./config");
 // -----------------------------------
 /* POST */
 //---------
-page.post("/hots", function (req, res) {
+page.get("/hots", function (req, res) {
   var sql =
     "SELECT `tb_main_article`.`articleno`, IFNULL(tb_user.nickname, SUBSTRING_INDEX(`tb_user`.`id`, '@', 1)) AS`username`,tb_main_article.`userno`, `title`,`image`,`avatar`, `view_count`, (SELECT COUNT(*) FROM `tb_collect` WHERE tb_collect.articleno=tb_main_article.articleno)AS `like_count`  FROM `tb_main_article` right JOIN `tb_user` on `tb_user`.`userno`=`tb_main_article`.`userno`  where  `tb_main_article`.`status`='show' ORDER BY `like_count` DESC;";
-  var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
+  // var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
 
-  connhelper.query(
-    sql + sqllike,
-    [req.body.userno],
-    function (err, result, fields) {
-      if (err) {
-        res.status("<HOT-熱門文章排序 Get>MySQL 可能語法寫錯了").send(err);
-      } else {
-        res.json(result);
-      }
+  connhelper.query(sql, [], function (err, result, fields) {
+    if (err) {
+      res.status("<HOT-熱門文章排序 Get>MySQL 可能語法寫錯了").send(err);
+    } else {
+      res.json(result);
     }
-  );
+  });
 });
 // -----------------------------------
 // Views 瀏覽數排序
 // -----------------------------------
 /* POST */
 //---------
-page.post("/views", function (req, res) {
+page.get("/views", function (req, res) {
   var sql =
     "SELECT `tb_main_article`.`articleno`, IFNULL(tb_user.nickname, SUBSTRING_INDEX(`tb_user`.`id`, '@', 1)) AS`username`,tb_main_article.`userno`, `title`,`image`,`avatar`, `view_count`, (SELECT COUNT(*) FROM `tb_collect` WHERE tb_collect.articleno=tb_main_article.articleno)AS `like_count`  FROM `tb_main_article` right JOIN `tb_user` on `tb_user`.`userno`=`tb_main_article`.`userno`  where  `tb_main_article`.`status`='show' ORDER BY `view_count` DESC;";
-  var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
+  // var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
 
-  connhelper.query(
-    sql + sqllike,
-    [req.body.userno],
-    function (err, result, fields) {
-      if (err) {
-        res.status("<HOT-熱門文章排序 Get>MySQL 可能語法寫錯了").send(err);
-      } else {
-        res.json(result);
-      }
+  connhelper.query(sql, [], function (err, result, fields) {
+    if (err) {
+      res.status("<HOT-熱門文章排序 Get>MySQL 可能語法寫錯了").send(err);
+    } else {
+      res.json(result);
     }
-  );
+  });
 });
 // -----------------------------------
 // Hashtags 前8個顯示
 // -----------------------------------
 /* GET */
 //---------
-page.get("/hashtags", function (req, res) {
-  var sql =
+page.post("/", function (req, res) {
+  var sqlA =
     "SELECT `tb_hashtag`.`tagno`, `tb_hashtag`.`hashtag`, count(`tb_article_hashtag`.`hashtagno`) as `count` FROM `tb_hashtag` JOIN `tb_article_hashtag` ON `tb_hashtag`.`tagno` = `tb_article_hashtag`.`hashtagno` WHERE `tb_hashtag`.`status` = 'T' GROUP BY `tb_hashtag`.`tagno` ORDER BY `count`DESC LIMIT 8;";
-  connhelper.query(sql, [], function (err, result, fields) {
-    if (err) {
-      res.status("<hashtag-8個 Get>MySQL 可能語法寫錯了").send(err);
-    } else {
-      res.json(result);
+  var sqlB =
+    "SELECT `tb_main_article`.`articleno`, IFNULL(tb_user.nickname, SUBSTRING_INDEX(`tb_user`.`id`, '@', 1)) AS`username`,tb_main_article.`userno`, `title`,`image`,`avatar`, `view_count`, (SELECT COUNT(*) FROM `tb_collect` WHERE tb_collect.articleno=tb_main_article.articleno)AS `like_count`  FROM `tb_main_article` right JOIN `tb_user` on `tb_user`.`userno`=`tb_main_article`.`userno`  where  `tb_main_article`.`status`='show' ORDER BY `view_count` DESC;";
+  var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
+
+  connhelper.query(
+    sqlA + sqlB + sqllike,
+    [req.body.userno],
+    function (err, result, fields) {
+      if (err) {
+        res.status("<hashtag-8個 Get>MySQL 可能語法寫錯了").send(err);
+      } else {
+        res.json(result);
+      }
     }
-  });
+  );
 });
 // -----------------------------------
 // Hashtags 被選中時，要顯示的文章
@@ -74,22 +74,18 @@ page.get("/hashtags", function (req, res) {
 page.post("/hashtags/:tagno", function (req, res) {
   var sql =
     "SELECT `tb_main_article`.`articleno`, IFNULL(tb_user.nickname, SUBSTRING_INDEX(`tb_user`.`id`, '@', 1)) AS`username`,tb_main_article.`userno`, `title`,`image`,`avatar`, `view_count`, (SELECT COUNT(*) FROM `tb_collect` WHERE tb_collect.articleno=tb_main_article.articleno)AS `like_count` FROM `tb_main_article` right JOIN `tb_article_hashtag` ON `tb_main_article`.`articleno` =`tb_article_hashtag`.`articleno` join `tb_user` on `tb_user`.`userno`=`tb_main_article`.`userno` where `tb_article_hashtag`.`hashtagno`=? AND `tb_main_article`.`status`='show' ORDER BY `view_count` DESC;";
-  var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
+  // var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
   // console.log(req.params);
   // console.log(req.body);
-  connhelper.query(
-    sql + sqllike,
-    [req.params.tagno, req.body.userno],
-    function (err, result, fields) {
-      if (err) {
-        res.status("<hashtag-選取之後POST> MySQL 可能語法寫錯了").send(err);
-      } else {
-        // console.log(result[0]);
-        // console.log(result[1]);
-        res.json(result);
-      }
+  connhelper.query(sql, [req.params.tagno], function (err, result, fields) {
+    if (err) {
+      res.status("<hashtag-選取之後POST> MySQL 可能語法寫錯了").send(err);
+    } else {
+      // console.log(result[0]);
+      // console.log(result[1]);
+      res.json(result);
     }
-  );
+  });
 });
 // -----------------------------------
 // location 被選中時，要顯示的文章
@@ -97,21 +93,18 @@ page.post("/hashtags/:tagno", function (req, res) {
 /* POST */
 //---------
 page.post("/city", function (req, res) {
+  console.log(req.body);
   var sql =
     "SELECT `tb_main_article`.`articleno`, IFNULL(tb_user.nickname, SUBSTRING_INDEX(`tb_user`.`id`, '@', 1)) AS`username` ,tb_main_article.`userno` , `title`,`image`,`avatar`, `view_count`, (SELECT COUNT(*) FROM `tb_collect` WHERE tb_collect.articleno=tb_main_article.articleno)AS `like_count`  FROM `tb_main_article` right JOIN `tb_user` on `tb_user`.`userno`=`tb_main_article`.`userno`  where `location`=? AND `tb_main_article`.`status`='show' ORDER BY `view_count` DESC;";
-  var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
-  connhelper.query(
-    sql + sqllike,
-    [req.body.city, req.body.userno],
-    function (err, result, fields) {
-      if (err) {
-        res.status("<city-選取之後POST> MySQL 可能語法寫錯了").send(err);
-      } else {
-        // console.log(result);
-        res.json(result);
-      }
+  // var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
+  connhelper.query(sql, [req.body.city], function (err, result, fields) {
+    if (err) {
+      res.status("<city-選取之後POST> MySQL 可能語法寫錯了").send(err);
+    } else {
+      console.log(result);
+      res.json(result);
     }
-  );
+  });
 });
 // -----------------------------------
 // searchbar 輸入關鍵字 要顯示的文章
@@ -121,15 +114,10 @@ page.post("/city", function (req, res) {
 page.post("/search", function (req, res) {
   var sql =
     "SELECT `tb_main_article`.`articleno`, IFNULL(tb_user.nickname, SUBSTRING_INDEX(`tb_user`.`id`, '@', 1)) AS`username`,tb_main_article.`userno`, `title`,`content`,`location`, `image`,`avatar`, `view_count`, (SELECT COUNT(*) FROM `tb_collect` WHERE tb_collect.articleno=tb_main_article.articleno)AS `like_count`  FROM `tb_main_article` right JOIN `tb_user` on `tb_user`.`userno`=`tb_main_article`.`userno`  where  `tb_main_article`.`status`='show' AND (tb_main_article.title LIKE ? OR location like ? OR content like ?) ORDER BY `like_count` DESC;";
-  var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
+  // var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
   connhelper.query(
-    sql + sqllike,
-    [
-      `%${req.body.search}%`,
-      `%${req.body.search}%`,
-      `%${req.body.search}%`,
-      req.body.userno,
-    ],
+    sql,
+    [`%${req.body.search}%`, `%${req.body.search}%`, `%${req.body.search}%`],
     function (err, result, fields) {
       if (err) {
         res.status("<search bar-輸入之後POST> MySQL 可能語法寫錯了").send(err);
