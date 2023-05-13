feccfc7b28cf09db8c7eb3c7359166990707251a
Tina <t2239tina@gmail.com>
2023-05-13 10:52:24 +0800

    合併重複sql

--- router/admin.js
+++ router/admin.js
@@ -7,24 +7,35 @@ var connhelper = require("./config");
 // -----------------------------------
 
 // -----------------------------------
-// 最新消息
+// First Render 最新消息＋會員＋文章
 // -----------------------------------
 /* GET */
 //---------
-page.get("/news", function (req, res) {
-  var sql =
+page.post("/", function (req, res) {
+  var sqlNews =
     "SELECT newsno,title, content, tb_news.status, DATE_FORMAT(`release`, '%Y-%m-%d') `release`,SUBSTRING_INDEX(`id`, '@', 1)`userno` FROM `tb_news` INNER JOIN `tb_user` on tb_news.userno=tb_user.userno ORDER BY `release` DESC;";
-  // "SELECT newsno,title, content, status, DATE_FORMAT(`release`, '%Y-%m-%d') `release`FROM `tb_news` ORDER BY `release` DESC;";
-  connhelper.query(sql, [], function (err, result, fields) {
-    if (err) {
-      res.status("<最新消息-渲染Get> MySQL 可能語法寫錯了").send(err);
-    } else {
-      res.json(result);
+  var sqlMembers =
+    "SELECT `userno`,`permission`,  `id`, `password`,  `nickname`, `birthday`,  `status` ,DATE_FORMAT(`date`, '%Y-%m-%d') `date` FROM `tb_user`; ";
+  var sqlUsername =
+    "SELECT  SUBSTRING_INDEX(`id`, '@', 1)`username` FROM `tb_user` WHERE userno=?;";
+  let sqlArticles =
+    "SELECT `articleno`,`userno`,`title`,`report_count`,`status`,`date` FROM `tb_main_article` WHERE `report_count`!=0 ORDER BY `articleno` DESC;";
+
+  connhelper.query(
+    sqlNews + sqlMembers + sqlUsername + sqlArticles,
+    [req.body.userno],
+    function (err, result, fields) {
+      if (err) {
+        res.status("<FirstRender> MySQL 可能語法寫錯了").send(err);
+      } else {
+        res.json(result);
+      }
     }
-  });
+  );
 });
+
 //---------
-/* POST */
+/* 最新消息-POST */
 //---------
 page.post("/news/post", function (req, res) {
   var sql =
@@ -47,7 +58,7 @@ page.post("/news/post", function (req, res) {
 });
 
 //---------
-/* PUT */
+/* 最新消息-PUT */
 //---------
 page.put("/news/update", function (req, res) {
   var sql =
@@ -70,7 +81,7 @@ page.put("/news/update", function (req, res) {
 });
 
 //---------
-/* DELETE */ //不刪除資料庫，僅是改變狀態
+/* 最新消息-DELETE */ //不刪除資料庫，僅是改變狀態
 //---------
 page.delete("/news/delete", function (req, res) {
   console.log(req.body);
@@ -96,29 +107,6 @@ page.delete("/news/delete", function (req, res) {
 // -----------------------------------
 // 會員管理
 // -----------------------------------
-/* GET */
-//---------
-page.get("/members", function (req, res) {
-  var sqlAll =
-    "SELECT `userno`,`permission`,  `id`, `password`,  `nickname`, `birthday`,  `status` ,DATE_FORMAT(`date`, '%Y-%m-%d') `date` FROM `tb_user`; ";
-  var sqlusername =
-    "SELECT  SUBSTRING_INDEX(`id`, '@', 1)`username` FROM `tb_user` WHERE userno=?;";
-
-  // console.log(req.query.userno);
-  connhelper.query(
-    sqlAll + sqlusername,
-    [req.query.userno],
-    function (err, result, fields) {
-      if (err) {
-        res.send("<會員管理-渲染get>MySQL 可能語法寫錯了", err);
-      } else {
-        res.json(result);
-      }
-    }
-  );
-});
-
-//---------
 /* PUT */
 //---------
 page.put("/members/update", function (req, res) {
@@ -137,36 +125,31 @@ page.put("/members/update", function (req, res) {
     }
   );
 });
+// -----------------------------------
+// 文章管理下架
+// -----------------------------------
+/* PUT */
 //---------
-/* 文章管理 */
-//---------
-page.get("/manageArtilcles", function (req, res) {
-  // res.send('done')
-  let sql =
-    "SELECT `articleno`,`userno`,`title`,`report_count`,`status`,`date` FROM `tb_main_article` WHERE `report_count`!=0 ORDER BY `articleno` DESC;";
-  connhelper.query(sql, [], function (err, results, fields) {
-    if (err) {
-      res.send("<文章管理-get>MySQL 可能語法寫錯了", err);
-    } else {
-      res.send(results);
-    }
-  });
-});
-
-// 文章管理下架＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 // update 文章狀態
 page.put("/takeOf", function (req, res) {
   var sql =
     "UPDATE `tb_main_article` SET `status`='report' WHERE `articleno`=?;";
   var sqlAll =
     "SELECT `articleno`,`userno`,`title`,`report_count`,`status`,`date` FROM `tb_main_article` WHERE `report_count`!=0 ORDER BY `articleno` DESC;";
-  connhelper.query(sql + sqlAll, [req.body.articleno], function (err, results, fields) {
-    if (err) {
-      res.send("<會員管理-更新put>MySQL 可能語法寫錯了", err);
-    } else {
-      res.send({ myalert: `${req.body.articleno}號文章已被刪除`, myresult: results[1] });
+  connhelper.query(
+    sql + sqlAll,
+    [req.body.articleno],
+    function (err, results, fields) {
+      if (err) {
+        res.send("<檢舉文章下架-更新put>MySQL 可能語法寫錯了", err);
+      } else {
+        res.send({
+          myalert: `${req.body.articleno}號文章已被刪除`,
+          myresult: results[1],
+        });
+      }
     }
-  });
+  );
 });
 
 module.exports = page;
--- router/articles.js
+++ router/articles.js
@@ -7,45 +7,9 @@ var connhelper = require("./config");
 // -----------------------------------
 
 // -----------------------------------
-// Hots 熱門度排序
+// First Render:Hashtags 前8個顯示+瀏覽數排序(預設)
 // -----------------------------------
 /* POST */
-//---------
-page.get("/hots", function (req, res) {
-  var sql =
-    "SELECT `tb_main_article`.`articleno`, IFNULL(tb_user.nickname, SUBSTRING_INDEX(`tb_user`.`id`, '@', 1)) AS`username`,tb_main_article.`userno`, `title`,`image`,`avatar`, `view_count`, (SELECT COUNT(*) FROM `tb_collect` WHERE tb_collect.articleno=tb_main_article.articleno)AS `like_count`  FROM `tb_main_article` right JOIN `tb_user` on `tb_user`.`userno`=`tb_main_article`.`userno`  where  `tb_main_article`.`status`='show' ORDER BY `like_count` DESC;";
-  // var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
-
-  connhelper.query(sql, [], function (err, result, fields) {
-    if (err) {
-      res.status("<HOT-熱門文章排序 Get>MySQL 可能語法寫錯了").send(err);
-    } else {
-      res.json(result);
-    }
-  });
-});
-// -----------------------------------
-// Views 瀏覽數排序
-// -----------------------------------
-/* POST */
-//---------
-page.get("/views", function (req, res) {
-  var sql =
-    "SELECT `tb_main_article`.`articleno`, IFNULL(tb_user.nickname, SUBSTRING_INDEX(`tb_user`.`id`, '@', 1)) AS`username`,tb_main_article.`userno`, `title`,`image`,`avatar`, `view_count`, (SELECT COUNT(*) FROM `tb_collect` WHERE tb_collect.articleno=tb_main_article.articleno)AS `like_count`  FROM `tb_main_article` right JOIN `tb_user` on `tb_user`.`userno`=`tb_main_article`.`userno`  where  `tb_main_article`.`status`='show' ORDER BY `view_count` DESC;";
-  // var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
-
-  connhelper.query(sql, [], function (err, result, fields) {
-    if (err) {
-      res.status("<HOT-熱門文章排序 Get>MySQL 可能語法寫錯了").send(err);
-    } else {
-      res.json(result);
-    }
-  });
-});
-// -----------------------------------
-// Hashtags 前8個顯示
-// -----------------------------------
-/* GET */
 //---------
 page.post("/", function (req, res) {
   var sqlA =
@@ -67,6 +31,41 @@ page.post("/", function (req, res) {
   );
 });
 // -----------------------------------
+// Hots 熱門度排序
+// -----------------------------------
+/* POST */
+//---------
+page.get("/hots", function (req, res) {
+  var sql =
+    "SELECT `tb_main_article`.`articleno`, IFNULL(tb_user.nickname, SUBSTRING_INDEX(`tb_user`.`id`, '@', 1)) AS`username`,tb_main_article.`userno`, `title`,`image`,`avatar`, `view_count`, (SELECT COUNT(*) FROM `tb_collect` WHERE tb_collect.articleno=tb_main_article.articleno)AS `like_count`  FROM `tb_main_article` right JOIN `tb_user` on `tb_user`.`userno`=`tb_main_article`.`userno`  where  `tb_main_article`.`status`='show' ORDER BY `like_count` DESC;";
+
+  connhelper.query(sql, [], function (err, result, fields) {
+    if (err) {
+      res.status("<HOT-熱門文章排序 Get>MySQL 可能語法寫錯了").send(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+// -----------------------------------
+// Views 瀏覽數排序
+// -----------------------------------
+/* POST */
+//---------
+page.get("/views", function (req, res) {
+  var sql =
+    "SELECT `tb_main_article`.`articleno`, IFNULL(tb_user.nickname, SUBSTRING_INDEX(`tb_user`.`id`, '@', 1)) AS`username`,tb_main_article.`userno`, `title`,`image`,`avatar`, `view_count`, (SELECT COUNT(*) FROM `tb_collect` WHERE tb_collect.articleno=tb_main_article.articleno)AS `like_count`  FROM `tb_main_article` right JOIN `tb_user` on `tb_user`.`userno`=`tb_main_article`.`userno`  where  `tb_main_article`.`status`='show' ORDER BY `view_count` DESC;";
+
+  connhelper.query(sql, [], function (err, result, fields) {
+    if (err) {
+      res.status("<HOT-熱門文章排序 Get>MySQL 可能語法寫錯了").send(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+// -----------------------------------
 // Hashtags 被選中時，要顯示的文章
 // -----------------------------------
 /* POST */
@@ -74,9 +73,6 @@ page.post("/", function (req, res) {
 page.post("/hashtags/:tagno", function (req, res) {
   var sql =
     "SELECT `tb_main_article`.`articleno`, IFNULL(tb_user.nickname, SUBSTRING_INDEX(`tb_user`.`id`, '@', 1)) AS`username`,tb_main_article.`userno`, `title`,`image`,`avatar`, `view_count`, (SELECT COUNT(*) FROM `tb_collect` WHERE tb_collect.articleno=tb_main_article.articleno)AS `like_count` FROM `tb_main_article` right JOIN `tb_article_hashtag` ON `tb_main_article`.`articleno` =`tb_article_hashtag`.`articleno` join `tb_user` on `tb_user`.`userno`=`tb_main_article`.`userno` where `tb_article_hashtag`.`hashtagno`=? AND `tb_main_article`.`status`='show' ORDER BY `view_count` DESC;";
-  // var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
-  // console.log(req.params);
-  // console.log(req.body);
   connhelper.query(sql, [req.params.tagno], function (err, result, fields) {
     if (err) {
       res.status("<hashtag-選取之後POST> MySQL 可能語法寫錯了").send(err);
@@ -96,7 +92,6 @@ page.post("/city", function (req, res) {
   console.log(req.body);
   var sql =
     "SELECT `tb_main_article`.`articleno`, IFNULL(tb_user.nickname, SUBSTRING_INDEX(`tb_user`.`id`, '@', 1)) AS`username` ,tb_main_article.`userno` , `title`,`image`,`avatar`, `view_count`, (SELECT COUNT(*) FROM `tb_collect` WHERE tb_collect.articleno=tb_main_article.articleno)AS `like_count`  FROM `tb_main_article` right JOIN `tb_user` on `tb_user`.`userno`=`tb_main_article`.`userno`  where `location`=? AND `tb_main_article`.`status`='show' ORDER BY `view_count` DESC;";
-  // var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
   connhelper.query(sql, [req.body.city], function (err, result, fields) {
     if (err) {
       res.status("<city-選取之後POST> MySQL 可能語法寫錯了").send(err);
@@ -114,7 +109,6 @@ page.post("/city", function (req, res) {
 page.post("/search", function (req, res) {
   var sql =
     "SELECT `tb_main_article`.`articleno`, IFNULL(tb_user.nickname, SUBSTRING_INDEX(`tb_user`.`id`, '@', 1)) AS`username`,tb_main_article.`userno`, `title`,`content`,`location`, `image`,`avatar`, `view_count`, (SELECT COUNT(*) FROM `tb_collect` WHERE tb_collect.articleno=tb_main_article.articleno)AS `like_count`  FROM `tb_main_article` right JOIN `tb_user` on `tb_user`.`userno`=`tb_main_article`.`userno`  where  `tb_main_article`.`status`='show' AND (tb_main_article.title LIKE ? OR location like ? OR content like ?) ORDER BY `like_count` DESC;";
-  // var sqllike = "SELECT `articleno` FROM `tb_collect` WHERE `userno`=?;";
   connhelper.query(
     sql,
     [`%${req.body.search}%`, `%${req.body.search}%`, `%${req.body.search}%`],
