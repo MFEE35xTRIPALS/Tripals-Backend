4c9e68b647a27204d2a945f291ec58d94a0197ff
Tina <t2239tina@gmail.com>
2023-05-19 00:19:15 +0800

    user圖片新增

--- router/login/register.js
+++ router/login/register.js
@@ -11,33 +11,37 @@ page.post("/register2", express.urlencoded(), (req, res) => {
   // }
 
   const sqlselect = "SELECT * FROM tb_user WHERE id = ?";
-  const sqlinsert = "INSERT INTO tb_user (id) VALUES (?);";
+  const sqlinsert = "INSERT INTO tb_user (id,password) VALUES (?,?);";
 
-  mysqlConn.query(sqlselect, [req.body.id], function (error, results) {
-    //判斷密碼二次輸入
-    if (req.body.password !== req.body.password2) {
-      console.log("輸入的密碼不一致請重新輸入");
-      return res.send("輸入的密碼不一致請重新輸入");
-    }
-    //判斷是否有被註冊
-    if (results.length > 0) {
-      console.log("已經被註冊");
-      return res.json();
-    } else {
-      mysqlConn.query(
-        sqlinsert,
-        [req.body.id, req.body.password],
-        function (err, results) {
-          if (err) {
-            console.error("DB error:", err);
-          } else {
-            console.log("註冊成功");
-            res.end();
+  mysqlConn.query(
+    sqlselect,
+    [req.body.id, req.body.password, req.body.password2],
+    function (error, results) {
+      //判斷密碼二次輸入
+      if (req.body.password !== req.body.password2) {
+        console.log("輸入的密碼不一致請重新輸入");
+        return res.send("輸入的密碼不一致請重新輸入");
+      }
+      //判斷是否有被註冊
+      if (results.length > 0) {
+        console.log("已經被註冊");
+        return res.json();
+      } else {
+        mysqlConn.query(
+          sqlinsert,
+          [req.body.id, req.body.password],
+          function (err, results) {
+            if (err) {
+              console.error("DB error:", err);
+            } else {
+              console.log("註冊成功");
+              res.end();
+            }
           }
-        }
-      );
+        );
+      }
     }
-  });
+  );
 });
 
 module.exports = page;
